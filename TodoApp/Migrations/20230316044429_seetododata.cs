35324cb9664012e3f24841ab557a23aa8a096c20
ABCabriga <57969408+ABCabriga@users.noreply.github.com>
2023-03-17 14:05:27 +0800

    Add project files.

---
+++ TodoApp/Migrations/20230316044429_seetododata.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    public partial class seetododata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DOB",
+                value: new DateTime(2003, 3, 16, 12, 44, 28, 910, DateTimeKind.Local).AddTicks(8501));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 17, 12, 44, 28, 910, DateTimeKind.Local).AddTicks(8612));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 18, 12, 44, 28, 910, DateTimeKind.Local).AddTicks(8623));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 18, 12, 44, 28, 910, DateTimeKind.Local).AddTicks(8624));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DOB",
+                value: new DateTime(2003, 3, 16, 12, 37, 9, 168, DateTimeKind.Local).AddTicks(1384));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 17, 12, 37, 9, 168, DateTimeKind.Local).AddTicks(1478));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 18, 12, 37, 9, 168, DateTimeKind.Local).AddTicks(1488));
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DueDate",
+                value: new DateTime(2023, 3, 18, 12, 37, 9, 168, DateTimeKind.Local).AddTicks(1489));
+        }
+    }
+}
---
+++ TodoApp/Repository/MsSQL/TodoDBRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Repository.MsSQL
+{
+    public class TodoDBRepository : ITodoRepository
+    {
+        TodoDbContext _dbContext;
+
+        public TodoDBRepository(TodoDbContext dbContext)
+        { 
+            _dbContext = dbContext;
+        }
+
+        public Todo AddTodo(Todo newTodo)
+        {
+            _dbContext.Add(newTodo);
+            _dbContext.SaveChanges();
+            return newTodo;
+        }
+
+        public Todo DeleteTodo(int todoId)
+        {
+            var todo = GetTodoById(todoId);
+            if (todo != null)
+            {
+                _dbContext.Todos.Remove(todo);
+                _dbContext.SaveChanges();
+            }
+            return todo;
+        }
+
+        public List<Todo> GetAllTodos()
+        {
+            return _dbContext.Todos.AsNoTracking().ToList();
+        }
+
+        public Todo GetTodoById(int Id)
+        {
+            return _dbContext.Todos.AsNoTracking().ToList().FirstOrDefault(t => t.Id == Id);
+        }
+
+        public Todo UpdateTodo(int todoId, Todo newTodo)
+        {
+            _dbContext.Todos.Update(newTodo);
+            _dbContext.SaveChanges();
+            return newTodo;
+        }
+    }
+}
